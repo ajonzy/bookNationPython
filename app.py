683785188a0b948203546b3917bcd8efcc7650e8
alexkessinger <alexkess101@gmail.com>
2019-03-27 10:53:59 -0600

    finished routes on Books class

--- app.py
+++ app.py
@@ -14,18 +14,20 @@ db = SQLAlchemy(app)
 
 # Classes go here
 class Book(db.Model):
+    __tablename__ = "book"
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(40), nullable=False)
-    spanish_title = db.Column(db.String(40), nullable=False)
-    author = db.Column(db.String(40), nullable=False)
+    title = db.Column(db.String(180), nullable=False)
+    spanish_title = db.Column(db.String(180), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
     cost = db.Column(db.Float, nullable=False)
-    genre = db.Column(db.String(40), nullable=False)
-    spanish_genre = db.Column(db.String(40), nullable=False)
-    summary = db.Column(db.String(5000), nullable=False)
-    spanish_summary = db.Column(db.String(5000), nullable=False)
-    cart_item = db.relationship('Cart_item', backref='book', lazy=True)
+    genre = db.Column(db.String(100), nullable=False)
+    spanish_genre = db.Column(db.String(100), nullable=False)
+    summary = db.Column(db.String(), nullable=False)
+    spanish_summary = db.Column(db.String(), nullable=False)
+    # cart_items = db.relationship('Cart_item', backref='book', lazy=True)
+    # order_items = db.relationship('Order_item', backref='book', lazy=True)
 
-    def __init__ (self, title, spanish_title, author, cost, genre, spanish_genre, summary, spanish_summary, cart_item):
+    def __init__ (self, title, spanish_title, author, cost, genre, spanish_genre, summary, spanish_summary):
         self.title = title
         self.spanish_title = spanish_title
         self.author = author
@@ -34,12 +36,46 @@ class Book(db.Model):
         self.spanish_genre = spanish_genre
         self.summary = summary
         self.spanish_summary = spanish_summary
-        self.cart_item = cart_item
 
 
     
 
 # Routes go here
+@app.route('/library', methods=['GET'])
+def return_library():
+    all_books = db.session.query(Book.id, Book.title, Book.spanish_title, Book.author, Book.cost, Book.genre, Book.spanish_genre, Book.summary, Book.spanish_summary).all()
+    return jsonify(all_books)
+
+@app.route('/library/input', methods=['POST'])
+def input_book():
+    if request.content_type == 'application/json':
+        post_data = request.get_json()
+        title = post_data.get('title')
+        spanish_title = post_data.get('spanish_title')
+        author = post_data.get('author')
+        cost = post_data.get('cost')
+        genre = post_data.get('genre')
+        spanish_genre = post_data.get('spanish_genre')
+        summary = post_data.get('summary')
+        spanish_summary = post_data.get('spanish_summary')
+        record = Book(title, spanish_title, author, cost, genre, spanish_genre, summary, spanish_summary)
+        db.session.add(record)
+        db.session.commit()
+        return jsonify("Data Posted")
+    return jsonify("Error adding Book")
+
+@app.route('/library/<id>', methods=["GET"])
+def return_book(id):
+    book = db.session.query(Book.title, Book.spanish_title, Book.author, Book.cost, Book.genre, Book.spanish_genre, Book.summary, Book.spanish_summary).filter(Book.id == id).first()
+    return jsonify(book)
+
+@app.route('/library/delete/<id>', methods=["DELETE"])
+def delete_book(id):
+    book = db.session.query(Book).get(id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify("Completed Delete action")
+
 
 
 if __name__ == "__main__":
