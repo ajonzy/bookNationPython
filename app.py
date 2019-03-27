5aa384a8aba7d6eb176be5b426748cf026751cdb
alexjones <ajonzy@gmail.com>
2019-03-27 09:27:40 -0600

    Added heroku database

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://mtzexrunaysute:87ff40992391ec6a4d3519c55741332fdf4017d09f9c2675b56d0fcdf86e03ea@ec2-54-225-129-101.compute-1.amazonaws.com:5432/da35vkc3i6vgao"
 
 heroku = Heroku(app)
 bcrypt = Bcrypt(app)
