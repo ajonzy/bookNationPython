6619841e06e60bce2b35200be57e88e39a3dd571
alexjones <ajonzy@gmail.com>
2019-03-27 09:20:24 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_heroku import Heroku
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+CORS(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = ""
+
+heroku = Heroku(app)
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
+# Classes go here
+
+
+# Routes go here
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
